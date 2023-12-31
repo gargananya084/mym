090df7ebd41731582b89ca0d11a46cf92e910155
gargananya084 <153811883+gargananya084@users.noreply.github.com>
2023-12-31 14:30:46 +0000

    variables

---
+++ 01_bascs/variables.js
@@ -0,0 +1,12 @@
+const accountId=1221//const value isnt changed.
+let name="Ananya"  //const let var keywords assign value in memory
+var surname="Garg"
+phone=12233121
+//use let not var=>block scope and function scope are more valid in new javascipt therefore let variables declared in varare not block oriented.
+console.table([accountId,name,surname,phone])
+//accountId=122211
+name="Rashik"
+surname="Dev"
+phone=22323
+let status
+console.table([accountId,name,surname,phone,status])
